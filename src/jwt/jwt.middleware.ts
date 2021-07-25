b34cf6a05bfafd22603302881319a27e12f6ba2c
inch <sxin2949@naver.com>
2021-07-25 19:44:25 +0900

    #5.7 JWT Middleware

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -1,15 +1,28 @@
-import { NestMiddleware } from "@nestjs/common";
+import { Injectable, NestMiddleware } from "@nestjs/common";
 import { NextFunction } from "express";
-import { RelationQueryBuilder } from "typeorm";
+import { JwtService } from "./jwt.service";
+import { UserService } from 'src/users/users.service';
 
-// export class JwtMiddleware implements NestMiddleware {  // implementsㄹㅏㅇ   
-//     use(req:Request, res: Response, next: NextFunction) {
-//         console.log(req.headers);
-//         next();
-//     }
-// }
+@Injectable()
+export class JwtMiddleware implements NestMiddleware { 
+    constructor(
+        private readonly jwtService: JwtService,
+        private readonly userService: UserService,
+    ) {} 
 
-export function jwtMiddleware(req:Request, res:Response, next:NextFunction) {
-    console.log(req.headers);
-    next();
+    async use(req:Request, res: Response, next: NextFunction) {
+        if("x-jwt" in req.headers) {
+            const token = req.headers['x-jwt'];
+            const decoded = this.jwtService.verify(token);
+            if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+              try {
+                const user = await this.userService.findById(decoded['id']);
+                req['user'] = user;
+                console.log(user);
+              } catch (e) {}
+            }
+            console.log(token);
+        }
+        next();
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { jwtMiddleware } from './jwt/jwt.middleware';
+import { JwtMiddleware } from './jwt/jwt.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
-  app.use(jwtMiddleware);   // 특정 구간에서 쓰고 싶으면 경로를 지정해줘야 하는데, consumer 방식으로 넣어야 함. #5.6 강의 참고.
+  // app.use(JwtMiddleware);   // 특정 구간에서 쓰고 싶으면 경로를 지정해줘야 하는데, consumer 방식으로 넣어야 함. #5.6 강의 참고.
+                               // 근데 이게 function 만 가능해서 결국 못 씀.. 
 }
 bootstrap();
